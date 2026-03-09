fcf421185486dbc959258d32b5d5b45b56f571fe
jayfourjavier <wryte4me@gmail.com>
2026-03-09 10:37:07 +0800

    update defines and secrets

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,6 @@
 
 // try to test pull requests
 
-#define TO_CALIBRATE false // set to true to run calibration routine, false to run normal operation
-
-#define MIXER_SPEED 20 // 0-100%
-
-// adjust pins if needed
-#define SCALE_CLK_PIN 6
-#define SCALE_DAT_PIN 7
-#define MOTOR_PWM_PIN 8
-#define HEATER_RELAY_PIN 9
-#define DHT_PIN 18
-#define DHT_TYPE DHT22
-
-#define SCALE_OFFSET 96462
-#define SCALE_CALIBRATION_FACTOR 168.916992
-
-#define ALAMANG 2000.0
-#define SALT 1000.0
 // #define RATIO (SALT / ALAMANG)  // COMPUTE RATIO BASED ON ALAMANG AND SALT WEIGHTS FROM TRIAL
 
 #define RATIO .5 // OR DEFINE EXPLICITLY IN CODE
---
+++ src/secrets.h
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+
+// Define your secrets here, such as WiFi credentials or API keys
+
+#define WIFI_SSID "yourWifiSsid"
+#define WIFI_PASSWORD "yourWifiPassword"
